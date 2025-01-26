244b168b1596d9488f4e369cbfa084c67cb0f228
Serg Anisko <horsm@mail.ru>
2025-01-26 22:07:55 +0300

    Write a Integration Test for Add Employee REST API

---
+++ src/test/java/com/example/springboot/controller/EmployeeControllerIntegrationTest.java
@@ -0,0 +1,43 @@
+package com.example.springboot.controller;
+
+import com.example.springboot.dto.EmployeeDto;
+import com.example.springboot.service.EmployeeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class EmployeeControllerIntegrationTest {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Autowired
+    private WebTestClient webClient;
+
+    @Test
+    public void testSaveEmployee() {
+
+        EmployeeDto employeeDto = new EmployeeDto();
+        employeeDto.setFirstName("Serg");
+        employeeDto.setLastName("Cina");
+        employeeDto.setEmail("serg.cina@gmail.com");
+
+        webClient.post().uri("/api/employees")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(employeeDto), EmployeeDto.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody()
+                .consumeWith(System.out::println)
+                .jsonPath("$.firstName").isEqualTo(employeeDto.getFirstName())
+                .jsonPath("$.lastName").isEqualTo(employeeDto.getLastName())
+                .jsonPath("$.email").isEqualTo(employeeDto.getEmail());
+    }
+}
